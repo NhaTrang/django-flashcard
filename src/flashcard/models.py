0bb7ef83c2ffc5c55fcb99fba02c9ff1d32f0b90
NhaTrang <arielis@hotmail.co.uk>
2017-03-17 00:48:22 +0100

    deprecated in django 1.9

--- src/flashcard/models.py
+++ src/flashcard/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.core.exceptions import ValidationError
 from django.db import models
 
